718c9fd04a39e9bf1acdb27b367ea645eb7d03b2
gr4v1ty69 <gr4v1ty69@users.noreply.github.com>
2026-02-06 16:15:08 +0100

    Update download URL and script source for user script

--- greasemonkey.user.js
+++ greasemonkey.user.js
@@ -14,12 +14,12 @@
 // @name strava-map-switcher
 // @description Map switcher for Strava website
 // @match https://www.strava.com/*
-// @downloadURL https://cdn.jsdelivr.net/gh/liskin/strava-map-switcher@master/greasemonkey.user.js
+// @downloadURL https://cdn.jsdelivr.net/gh/gr4v1ty69/strava-map-switcher-v2@master/greasemonkey.user.js
 // ==/UserScript==
 
 {
 	const s = document.createElement("script");
-	s.src = 'https://cdn.jsdelivr.net/gh/liskin/strava-map-switcher@master/load.js';
+	s.src = 'https://cdn.jsdelivr.net/gh/gr4v1ty69/strava-map-switcher-v2@master/load.js';
 	s.type = 'text/javascript';
 	document.body.appendChild(s);
 }
